1488faf9377b2c8cef55e3f31e66161680522929
Noah Thompson <noah1010@me.com>
2020-07-18 04:40:21 -0700

    HeightMap and LODs are finished

---
+++ Testing  Grounds/Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Drawing.Slots;
+using UnityEngine;
+using UnityEngine.AI;
+
+public static class MeshGenerator
+{
+    public static MeshData GenerateTerrainMesh(float[,] heightMap, float heightMultiplier, AnimationCurve meshHeightCurve,int levelOfDetail)
+    {
+        int width = heightMap.GetLength(0);
+        int height = heightMap.GetLength(1);
+        float topLeftX = (width - 1) / 2f;
+        float topLeftZ = (height - 1) / 2f;
+
+        int meshSimplificationIncrement = (levelOfDetail == 0) ? 1 : levelOfDetail * 2;
+        int vertsPerLine = (width - 1) / meshSimplificationIncrement + 1;
+
+        MeshData meshData = new MeshData(vertsPerLine, vertsPerLine);
+        int vertIndex = 0;
+
+        for (int y = 0; y < height; y+= meshSimplificationIncrement) 
+        {
+            for (int x = 0; x < width; x+= meshSimplificationIncrement) 
+            {
+                meshData.verts[vertIndex] = new Vector3(topLeftX + x, meshHeightCurve.Evaluate( heightMap[x, y] ) * heightMultiplier, topLeftZ - y);
+                meshData.uvs[vertIndex] = new Vector2(x / (float)width, y / (float)height);
+
+                if (x < width-1 && y<height-1)
+                {
+                    meshData.AddTriangle(vertIndex, vertIndex + vertsPerLine + 1, vertIndex + vertsPerLine);
+                    meshData.AddTriangle(vertIndex + vertsPerLine + 1, vertIndex, vertIndex + 1);
+
+                }
+
+                vertIndex++;
+            }
+        }
+
+        return meshData;
+    }
+}
+
+public class MeshData
+{
+    public Vector3[] verts;
+    public int[] triangles;
+    public Vector2[] uvs;
+
+    int triangleIndex;
+
+    public MeshData(int meshWidth,int meshHeight)
+    {
+        verts = new Vector3[meshWidth * meshHeight];
+        uvs = new Vector2[meshWidth * meshHeight];
+        triangles = new int[(meshWidth - 1) * (meshHeight - 1)*6];
+    }
+
+    public void AddTriangle(int a, int b, int c)
+    {
+        triangles[triangleIndex] = a;
+        triangles[triangleIndex + 1] = b;
+        triangles[triangleIndex + 2] = c;
+
+        triangleIndex += 3;
+    }
+
+    public Mesh CreateMesh()
+    {
+        Mesh mesh = new Mesh();
+        mesh.vertices = verts;
+        mesh.uv = uvs;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+        return mesh;
+    }
+}
---
+++ Testing  Grounds/Assets/Scripts/Noise.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderGraph;
+using UnityEngine;
+
+public static class Noise 
+{
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset)
+    {
+        float[,] noisemap = new float[mapWidth, mapHeight];
+
+        System.Random prng = new System.Random(seed);
+        Vector2[] octaveOffset = new Vector2[octaves]
+;
+        for(int ii = 0; ii<octaves;ii++)
+        {
+            float offsetX = prng.Next(-100000, 100000) + offset.x;
+            float offsetY = prng.Next(-100000, 100000) + offset.y;
+            octaveOffset[ii] = new Vector2(offsetX, offsetY);
+        }
+        if (scale <= 0)
+        {
+            scale = 0.0001f;
+        }
+
+        float maxNoiseHeight = float.MinValue;
+        float minNoiseHeight = float.MaxValue;
+
+        float halfwidth = mapWidth / 2f;
+        float halfHeight = mapHeight / 2f;
+
+
+        for(int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0;x<mapWidth;  x++)
+            {
+
+                float amplitude = 1;
+                float frequency = 1;
+                float noiseHeight = 0;
+
+                for (int ii = 0; ii < octaves; ii++)
+                {
+                    float sampleX = (x-halfwidth) / scale * frequency + octaveOffset[ii].x;
+                    float sampleY = (y-halfHeight) / scale * frequency + octaveOffset[ii].y;
+
+                    float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
+                    noiseHeight += perlinValue * amplitude;
+
+                    amplitude *= persistance;
+                    frequency *= lacunarity;
+                }
+
+                if(noiseHeight > maxNoiseHeight)
+                {
+                    maxNoiseHeight = noiseHeight;
+                }
+                else if(noiseHeight<minNoiseHeight)
+                {
+                    minNoiseHeight = noiseHeight;
+                }
+
+                noisemap[x, y] = noiseHeight;
+            }
+        }
+
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                noisemap[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noisemap[x, y]);
+            }
+        }
+
+                return noisemap;
+    }
+
+}
